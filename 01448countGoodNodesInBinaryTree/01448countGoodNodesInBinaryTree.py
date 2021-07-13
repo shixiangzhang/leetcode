604cf23f3aab2ac99a319f3c45e93e83a4cf95ae
Shixiang Zhang <shixiang.sean.zhang@gmail.com>
2021-07-13 00:00:15 -0400

    01448 Count Good Nodes In Binary Tree Solution improve

--- 01448countGoodNodesInBinaryTree/01448countGoodNodesInBinaryTree.py
+++ 01448countGoodNodesInBinaryTree/01448countGoodNodesInBinaryTree.py
@@ -8,8 +8,8 @@ class TreeNode:
          self.right = right
 
 #recursion
-#runtime o(n)
-class Solution:
+#runtime O(n^2), List Comprehensive part is O(n)
+class Solution1:
     def goodNodes(self, root: TreeNode) -> int:
         res = len(self.goodNodesList(root))
         return res
@@ -24,10 +24,29 @@ class Solution:
         res = leftList1 + rightList1 + [curRootVal]
         return res
 
+#recursion
+#runtime O(n)
+class Solution2:
+    def goodNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        self.res = 0 # good variable using self
+        curMax = root.val
+        def countGoodNodesWithMax(root: TreeNode,curMax:int) -> int: #no self in arguments
+            if not root:
+                return self.res          
+            if root.val>=curMax:
+                curMax = root.val
+                self.res += 1
+            countGoodNodesWithMax(root.left,curMax)
+            countGoodNodesWithMax(root.right,curMax)
+            return self.res
+        return countGoodNodesWithMax(root,curMax)
+
 
 if __name__=='__main__':
     root = TreeNode(val=3, left=TreeNode(val=1,left=TreeNode(val=3)), right=TreeNode(val=4, left=TreeNode(val=1), right=TreeNode(val=5)))
-    res = Solution()
+    res = Solution2()
     res.goodNodes(root)
     res.goodNodesList(root)
 
